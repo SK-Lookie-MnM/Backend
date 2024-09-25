f1b2c32d4b97666f73887909c609baa1ec899313
[Kkimdoyeon] <[ehdus5176@naver.com]>
2024-09-25 17:06:51 +0900

    feat: 도메인 생성 (User, Pass, Inquiry)

---
+++ mnm_backend/src/main/java/com/example/mnm_backend/domain/Pass.java
@@ -0,0 +1,29 @@
+package com.example.mnm_backend.domain;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Passes")
+public class Pass {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long passId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    //횟수권 구매 여부
+    @Column(nullable = false)
+    private Boolean passHolder;
+
+    @Column(nullable = true)
+    private LocalDate passStartDate;
+
+    @Column(nullable = true)
+    private LocalDate passEndDate;
+}
+
