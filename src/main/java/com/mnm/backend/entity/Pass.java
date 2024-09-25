1f89440813d956681a3032a77412252004846ec7
hyouzl <hyouzl0521@gmail.com>
2024-09-26 03:13:23 +0900

    Init: 프로젝트 초기 재설정

---
+++ src/main/java/com/mnm/backend/entity/Pass.java
@@ -0,0 +1,28 @@
+package com.mnm.backend.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Passes")
+public class Pass {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long passId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    //횟수권 구매 여부
+    @Column(nullable = false)
+    private Boolean passHolder;
+
+    @Column(nullable = true)
+    private LocalDate passStartDate;
+
+    @Column(nullable = true)
+    private LocalDate passEndDate;
+}
---
+++ src/main/java/com/mnm/backend/entity/User.java
@@ -0,0 +1,37 @@
+package com.mnm.backend.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "User")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false, length = 50)
+    private String nickname;
+
+    @Column(nullable = true)
+    private String university;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role = Role.MEMBER; // 기본값으로 'member' 설정
+
+    @Column(nullable = false, length = 100)
+    private String socialLoginId;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public enum Role {
+        ADMIN, MEMBER
+    }
+
+}
