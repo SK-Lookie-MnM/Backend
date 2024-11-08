15db0d1cda14c8c3dcf62f5348ab899d0f31626b
hyouzl <hyouzl0521@gmail.com>
2024-11-08 22:45:48 +0900

    fix: github action build 오류 해결(#9)

--- src/main/java/com/mnm/backend/config/SecurityConfig.java
+++ src/main/java/com/mnm/backend/config/SecurityConfig.java
@@ -1,7 +1,18 @@
 package com.mnm.backend.config;
 
+<<<<<<< Updated upstream
+=======
+
+import com.mnm.backend.auth.filter.JwtFilter;
+import com.mnm.backend.auth.jwt.JwtAccessDeniedHandler;
+import com.mnm.backend.auth.jwt.JwtAuthenticationEntryPoint;
+import com.mnm.backend.auth.util.TokenProvider;
+
+import org.springframework.beans.factory.annotation.Autowired;
+>>>>>>> Stashed changes
 import org.springframework.context.annotation.Bean;
 
+<<<<<<< Updated upstream
 //@EnableWebSecurity
 //public class SecurityConfig {
 //    @Bean
@@ -15,3 +26,69 @@ import org.springframework.context.annotation.Bean;
 //        return http.build();
 //    }
 //}
+=======
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    @Autowired
+    public SecurityConfig(TokenProvider tokenProvider, JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint, JwtAccessDeniedHandler jwtAccessDeniedHandler) {
+        this.tokenProvider = tokenProvider;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        JwtFilter jwtFilter = new JwtFilter(tokenProvider);
+        http
+                .cors(Customizer.withDefaults()) // CORS 설정 추가
+                .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(exceptionHandling ->
+                        exceptionHandling
+                                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                                .accessDeniedHandler(jwtAccessDeniedHandler)
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("api/users/login","api/users/reissue","/api/users/signup", "/api/users/checkEmail", "/api/users/checkLoginId", "/error").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .headers(
+                        headersConfigurer ->
+                                headersConfigurer
+                                        .frameOptions(
+                                                HeadersConfigurer.FrameOptionsConfig::sameOrigin
+                                        )
+                )
+                .httpBasic(httpBasic -> httpBasic.disable()); // JwtSecurityConfig 적용
+
+        ;// HTTP Basic 인증 비활성화
+        return http.build();
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:8080")); // 허용할 도메인 설정
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
+>>>>>>> Stashed changes
